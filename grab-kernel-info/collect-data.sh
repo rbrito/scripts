84d60b37729308aa2224d3679eeb9707de8b235d
Rogério Brito <rbrito@ime.usp.br>
2012-06-22 07:38:54 -0300

    (WIP) Add script to collect data about a system.

---
+++ grab-kernel-info/collect-data.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DATE=$(date +%s)
+KVER=$(uname -r)
+DIR=kernel-debug-$DATE
+mkdir $DIR
+cd $DIR
+
+cp /boot/config-$KVER		.
+cp /proc/cpuinfo		proc-cpuinfo.txt
+cp /proc/interrupts		proc-interrupts.txt
+cp /proc/iomem			proc-iomem.txt
+cp /proc/ioports		proc-ioports.txt
+cp /var/log/Xorg.0.log		.
+
+uname -a			> uname-a.txt
+dmesg				> $DATE-dmesg-$KVER.log
+acpidump			> acpidump.txt
+dmidecode			> dmidecode.txt
+
+lspci -v			> lspci-v.txt
+lspci -vvxx			> lspci-vvxx.txt
+lspci -vx			> lspci-vx.txt
+lspci				> lspci.txt
+lsusb				> lsusb.txt
+lsmod				> lsmod.txt
+
+lshw				> lshw.txt
+
+mount -t debugfs none /sys/kernel/debug/
+tar cvf debugfs-$DATE.tar /sys/kernel/debug
+
+cd ..
+tar zcvf $DIR.tar.gz $DIR
