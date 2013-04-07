aa565247c7fd34e02b939c7e5c228e38b706559a
Rogério Brito <rbrito@ime.usp.br>
2013-04-07 06:26:26 -0300

    First (non-working) attempt at a plugin for qbittorrent.

--- engine_name.py
+++ engine_name.py
@@ -1,34 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from novaprinter import prettyPrinter
-from helpers import retrieve_url
+from helpers import retrieve_url, download_file
+from lxml import etree
+
+import lxml.html.soupparser
+
+import logging
+
 
 class engine_name(object):
-  url = 'http://psychocydd.co.uk'
-  name = 'RockBox'
+    url = 'http://psychocydd.co.uk'
+    name = 'RockBox'
 
-  supported_categories = {
-    'all': '0',
-    'movies': '6',
-    'music': '1',
-    }
+    supported_categories = {'all': '0', 'movies': '6', 'music': '1'}
 
-  def __init__(self):
-    # some initialization
-    pass
+    @staticmethod
+    def bytes_from_string(s):
+        """
+        We assume that there is a space between the numbers and the units.
+        """
+        sz = s.split()
+        return int(float(sz[0]) * (1024 ** ' BKMG'.index(sz[1][0])))
 
-  def download_torrent(self, info):
-    # Providing this function is optional. It can however be interesting to provide
-    # your own torrent download implementation in case the search engine in question
-    # does not allow traditional downloads (for example, cookie-based download).
-    print download_file(info)
 
-  # DO NOT CHANGE the name and parameters of this function
-  # This function will be the one called by nova2.py
-  def search(self, what, cat='all'):
-    # what is a string with the search tokens, already escaped (e.g. "Ubuntu+Linux")
-    # cat is the name of a search category in ('all', 'movies', 'tv', 'music', 'games', 'anime', 'software', 'pictures', 'books')
-    #
-    # Here you can do what you want to get the result from the
-    # search engine website.
-    # everytime you parse a result line, store it in a dictionary
-    # and call the prettyPrint(your_dict) function
-    pass
+    def __init__(self):
+        logging.error('Class created.')
+
+    # def download_torrent(self, info):
+    #     # Providing this function is optional. It can however be interesting
+    #     # to provide your own torrent download implementation in case the
+    #     # search engine in question does not allow traditional downloads
+    #     # (for example, cookie-based download).
+    #     print(download_file(info))
+
+    # DO NOT CHANGE the name and parameters of this function This
+    # function will be the one called by nova2.py
+    def search(self, what, cat='all'):
+        """
+        Method called by nova2.
+
+        `what` is the already scaped search string, while `cat` restricts in
+        which category the search should be performed.
+
+        For each parsed line of the result, we put it in a dictionary and
+        pass it to the prettyPrint function.
+        """
+        logging.error('Searching for %s.', what)
+
+        data = retrieve_url('http://psychocydd.co.uk/torrents.php?search=%s' % what)
+
+        logging.error('Data retrieved.')
+        with open('searchresult.html', 'w') as f:
+            f.write(data)
+
+        logging.error('Creating tree.')
+
+        #htmlparser = etree.HTMLParser()
+        #tree = etree.fromstring(data, htmlparser)
+        tree = lxml.html.soupparser.fromstring(data)
+        table = tree.xpath('/html/body/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr')
+
+        logging.error('Tree created.')
+
+        # Relevant <tr>s have length 12.
+        for row in table:
+            if len(row) == 12:
+                logging.error('Found relevant row.')
+
+                info = {
+                    'name': row[1][0].text,
+                    'link': row[4][0].get('href'),
+                    'size': bytes_from_string(row[6].text),
+                    'seeds': int(row[7][0].text),
+                    'leech': int(row[8][0].text),
+                    'engine_url': self.url,
+                    }
+                prettyPrinter(info)
