92bc9dd6b3d2827b2af721c9b25fb1b9030a382b
Rogério Brito <rbrito@ime.usp.br>
2017-01-07 04:46:53 -0200

    repack-epub: Don't call programs if there are no arguments.
    Otherwise, optipng, advpng, advdef, jpgcrush etc. get confused.

--- repack-epub.sh
+++ repack-epub.sh
@@ -16,19 +16,19 @@ unzip "$REALPATH"
 
 # Fix permissions; I've seen them wrong when unzipping an epub and
 # they prevent all the next steps from being performed.
-find . -type d -print0 | xargs -0 chmod 755
-find . -type f -print0 | xargs -0 chmod 644
+find . -type d -print0 | xargs -0 -r chmod 755
+find . -type f -print0 | xargs -0 -r chmod 644
 
 # Optimize PNGs
-find . -iname "*.png" -print0 | xargs -0 optipng -o4
-find . -iname "*.png" -print0 | xargs -0 advpng -z3
-find . -iname "*.png" -print0 | xargs -0 advpng -z4
-find . -iname "*.png" -print0 | xargs -0 advdef -z3
-find . -iname "*.png" -print0 | xargs -0 advdef -z4
+find . -iname "*.png" -print0 | xargs -0 -r optipng -o4
+find . -iname "*.png" -print0 | xargs -0 -r advpng -z3
+find . -iname "*.png" -print0 | xargs -0 -r advpng -z4
+find . -iname "*.png" -print0 | xargs -0 -r advdef -z3
+find . -iname "*.png" -print0 | xargs -0 -r advdef -z4
 
 # Optimize JPGs
-find . \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 jpgcrush
-find . \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 jhead -purejpg
+find . \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 -r jpgcrush
+find . \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 -r jhead -purejpg
 
 # Repack the epub. NOTE: The order and the options *ARE* important.
 NEWNAME="$ORIGDIR/${ORIGNAME%%epub}new.epub"
