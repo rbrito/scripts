df46e17b9fad5a55995547c7dfb035bbc6b00275
Rogério Brito <rbrito@ime.usp.br>
2018-03-13 03:44:22 -0300

    convert_timestamps_to_chapters: Add script to convert timestamps from YouTube to chapter files.

---
+++ convert_timestamps_to_chapters.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import re
+import sys
+
+lines = sys.stdin.readlines()
+
+for lineno, line in enumerate(lines):
+    m = re.match('(?P<hours>\d+:)?(?P<minutes>\d+):(?P<seconds>\d+) (?P<title>.*)', line)
+    if m:
+        parts = m.groupdict()
+        if parts['hours']:
+            hours = int(parts['hours'].replace(':', '')) # clean with better regexp
+        else:
+            hours = 0
+        minutes = int(parts['minutes'])
+        seconds = int(parts['seconds'])
+        # print(hours)
+        # print(minutes)
+        # print(seconds)
+        
+        print('CHAPTER%02d=%02d:%02d:%02d.000' % (lineno, hours, minutes, seconds))
+        print('CHAPTER%02dNAME=%s' % (lineno, parts['title']))
