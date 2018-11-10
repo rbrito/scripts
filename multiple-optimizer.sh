bfc5e50e47c7f691352ef21ea67074adf6b4f64e
Rogério Brito <rbrito@ime.usp.br>
2018-11-10 13:34:19 -0200

    multiple-optimizer: Add 'profiles' to compress.

--- multiple-optimizer.sh
+++ multiple-optimizer.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
+set -e
+
 OPTIMIZER=~/Downloads/pdfsizeopt/pdfsizeopt
+
+case $1 in
+    --help)
+        echo "$0 --optimize-{ultrafast,fast,normal,extra,ultra} <input.pdf>"
+        exit 0
+        ;;
+    --optimize-ultrafast)
+        IMAGE_OPTIMIZERS=jbig2
+        shift
+        ;;
+    --optimize-fast)
+        IMAGE_OPTIMIZERS=rbrito0,jbig2
+        shift
+        ;;
+    --optimize-normal)
+        IMAGE_OPTIMIZERS=rbrito,jbig2
+        shift
+        ;;
+    --optimize-extra)
+        IMAGE_OPTIMIZERS=pngout,rbrito3,jbig2
+        shift
+        ;;
+    --optimize-ultra)
+        IMAGE_OPTIMIZERS=zopflipng,pngout3,rbrito3,jbig2
+        shift
+        ;;
+    *)
+        IMAGE_OPTIMIZERS=rbrito,jbig2
+esac
+
 
 qpdf --stream-data=uncompress --compress-streams=n --decode-level=specialized "$1" "${1%%pdf}unc.pdf"
 
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=no "${1%%pdf}unc.pdf"
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=yes --do-optimize-images=no "${1%%pdf}unc.pso.pdf"
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=no --do-optimize-images=no "${1%%pdf}unc.pso.psom.pdf"
 
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=no "${1%%pdf}unc.pdf"
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=yes --do-optimize-images=no "${1%%pdf}unc.pso.pdf"
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=no --do-optimize-images=no "${1%%pdf}unc.pso.psom.pdf"
-
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=no "${1%%pdf}pdf"
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=yes --do-optimize-images=no "${1%%pdf}pso.pdf"
-$OPTIMIZER --use-image-optimizer=rbrito,jbig2 --use-multivalent=no --do-optimize-images=no "${1%%pdf}pso.psom.pdf"
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=no "${1%%pdf}pdf"
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=yes --do-optimize-images=no "${1%%pdf}pso.pdf"
+$OPTIMIZER --use-image-optimizer=$IMAGE_OPTIMIZERS --use-multivalent=no --do-optimize-images=no "${1%%pdf}pso.psom.pdf"
