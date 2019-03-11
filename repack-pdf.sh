608cccf5c6219542e97eafa6d8fe7370de89d9c7
Rogério Brito <rbrito@ime.usp.br>
2019-03-11 04:38:45 -0300

    repack-pdf: Add a script to repack a PDF made only of images.
    Each page has to be just one image. If a page is composed of multiple
    images, perhaps the concat-pdf-images script may help (but only in very
    specific cases).
    
    This script needs a lot more robustness work.

--- repack-pdf.sh
+++ repack-pdf.sh
@@ -3,13 +3,13 @@
 mkdir temp
 cd temp
 
-pdfimages -all ../"$1" a
+pdfimages -tiff ../"$1" a
 jhead -purejpg *.jpg
 jpgcrush *.jpg
 pingo -lossless -s9 -verbose=3 *.png
 
 
-img2pdf --verbose --pagesize A4 -o ../"${1%%pdf}repack.pdf" *
+img2pdf --verbose -o ../"${1%%pdf}repack.pdf" *
 
-cd ..
-rm -rf temp
+#cd ..
+#rm -rf temp
