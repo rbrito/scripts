b6f35fbb906bf4c1f8b4986dde2c51dba21f33cb
Rogério Brito <rbrito@ime.usp.br>
2019-03-20 21:57:02 -0300

    repack-pdf: Add commented line with command to set DPI of images.

--- repack-pdf.sh
+++ repack-pdf.sh
@@ -8,6 +8,7 @@ jhead -purejpg *.jpg
 jpgcrush *.jpg
 pingo -lossless -s9 -verbose=3 *.png
 
+# exiftool -Xresolution=600 -Yresolution=600 -ResolutionUnit=inches *.tif
 
 img2pdf --verbose -o ../"${1%%pdf}repack.pdf" *
 
