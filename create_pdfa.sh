de601437051a2da45cf6bfadf6a6f0400f06d9c7
Rogério Brito <rbrito@ime.usp.br>
2019-07-11 18:07:25 -0300

    create_pdfa: Make PDF/A script hopefully more robust.

--- create_pdfa.sh
+++ create_pdfa.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-# 'gs', '-dQUIET', '-dBATCH', '-dNOPAUSE', '-dCompatibilityLevel=1.6', '-dNumRenderingThreads=4', '-sDEVICE=pdfwrite', '-dAutoRotatePages=/None', '-sColorConversionStrategy=RGB', '-dAutoFilterColorImages=true', '-dAutoFilterGrayImages=true', '-dJPEGQ=95', '-dPDFA=2', '-dPDFACompatibilityPolicy=1', '-sOutputFile=/home/rbrito/tmp/tmp2pt27bs_', '/home/rbrito/tmp/com.github.ocrmypdf.k0hymyks/layers.rendered.pdf', '/home/rbrito/tmp/com.github.ocrmypdf.k0hymyks/pdfa.ps'
-
-ps2pdf -dQUIET -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dNumRenderingThreads=4 -dAutoRotatePages=/None -sColorConversionStrategy=RGB -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dJPEGQ=95 -dPDFA=2 -dPDFACompatibilityPolicy=1 "$1" ~/bin/pdfa.ps
+gs -dQUIET -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dNumRenderingThreads=4 -dAutoRotatePages=/None -sColorConversionStrategy=RGB -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dJPEGQ=95 -dPDFA=2 -sOutputFile="${1%.*}.pdfa.pdf" -dPDFACompatibilityPolicy=1 "$1" ~/bin/pdfa.ps
