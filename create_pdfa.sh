8b219f38405f274de61a88c2fdf0bad3e2b80fb7
Rogério Brito <rbrito@ime.usp.br>
2019-09-27 16:41:55 -0300

    create_pdfa: Put each option in one line, for readability.

--- create_pdfa.sh
+++ create_pdfa.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-gs -dQUIET -dBATCH -dNOPAUSE -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.6 -dNumRenderingThreads=4 -dAutoRotatePages=/None -sColorConversionStrategy=RGB -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dJPEGQ=95 -dPDFA=2 -sOutputFile="${1%.*}.pdfa.pdf" -dPDFACompatibilityPolicy=1 "$1" ~/bin/pdfa.ps
+gs \
+    -dQUIET \
+    -dBATCH \
+    -dNOPAUSE \
+    -dSAFER \
+    -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.6 \
+    -dNumRenderingThreads=4 \
+    -dAutoRotatePages=/None \
+    -sColorConversionStrategy=RGB \
+    -dAutoFilterColorImages=true \
+    -dAutoFilterGrayImages=true \
+    -dJPEGQ=95 \
+    -dPDFA=2 \
+    -sOutputFile="${1%.*}.pdfa.pdf" \
+    -dPDFACompatibilityPolicy=1 \
+    "$1" \
+    ~/bin/pdfa.ps
