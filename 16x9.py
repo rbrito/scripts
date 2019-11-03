76adaa9fce3cd8d808484eb3457ef3716ae91fe4
Rogério Brito <rbrito@ime.usp.br>
2019-11-03 20:17:07 -0300

    16x9: Port to Python 3.

--- 16x9.py
+++ 16x9.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 for j in [16, 8, 4, 2]:
-    print "mod ", j
+    print("mod ", j)
     for i in range(1, 1000):
         h = i*j
         if h % 9 == 0:
-            print "*", h*16/9, "x", h
+            print("*", h*16//9, "x", h)
