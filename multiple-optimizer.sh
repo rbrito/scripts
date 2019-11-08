fa04dbfffeed5fd2280945d9da35e6eeaef29b6a
Rogério Brito <rbrito@ime.usp.br>
2019-11-08 09:44:23 -0300

    multiple-optimizer: Use more mnemonic levels.

--- multiple-optimizer.sh
+++ multiple-optimizer.sh
@@ -6,26 +6,26 @@ OPTIMIZER=~/Downloads/pdfsizeopt/pdfsizeopt
 
 case $1 in
     --help)
-        echo "$0 --optimize-{ultrafast,fast,normal,extra,ultra} <input.pdf>"
+        echo -e "$0 -{1,2,3,4,5} <input.pdf>\nLevel 3 is the default."
         exit 0
         ;;
-    --optimize-ultrafast)
+    -1)
         IMAGE_OPTIMIZERS=jbig2
         shift
         ;;
-    --optimize-fast)
+    -2)
         IMAGE_OPTIMIZERS=rbrito0,jbig2
         shift
         ;;
-    --optimize-normal)
+    -3)
         IMAGE_OPTIMIZERS=pingo9,rbrito,jbig2
         shift
         ;;
-    --optimize-extra)
+    -4)
         IMAGE_OPTIMIZERS=pingo9,pngout,rbrito3,jbig2
         shift
         ;;
-    --optimize-ultra)
+    -5)
         IMAGE_OPTIMIZERS=pingo9+extra,zopflipng,pngout3,rbrito3,jbig2
         shift
         ;;
