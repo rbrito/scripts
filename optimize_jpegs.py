0e92377d7c3d57bfc621cc1d0cb34f8a43027138
Rogério Brito <rbrito@ime.usp.br>
2020-01-09 03:51:09 -0300

    optimize_jpegs: Use a generator to make iteration more pythonic.
    This also allows us to remove one level of indentation, which makes the code
    more readable.
    
    The best alternative would be for the pikepdf library to expose iteration of
    the images out-of-the-box. This would execute native code instead of
    executing interpreted (Python) code.
    
    Another (better?) alternative would be to write a derived/adapter class
    based on pikepdf that has this generator/iterator of image objects as a
    method, but it is probably not worth the effort (this needs to be
    evaluated).

--- optimize_jpegs.py
+++ optimize_jpegs.py
@@ -8,6 +8,11 @@ import tempfile
 
 import pikepdf
 
+# Generator to make the following code more pythonic
+def image_objects(pdf):
+    for obj in pdf.objects:
+        if isinstance(obj, pikepdf.Stream) and '/Subtype' in obj and obj['/Subtype'] == '/Image':
+            yield obj
 
 
 def main(tmpdirname, pdf_name):
@@ -15,54 +20,50 @@ def main(tmpdirname, pdf_name):
 
     my_pdf = pikepdf.open(pdf_name)
 
-    for obj in my_pdf.objects:
+    for image_obj in image_objects(my_pdf):
 
-        # Not elegant, but (hopefully) will be made so in the near future
-        if isinstance(obj, pikepdf.Stream) and '/Subtype' in obj and obj['/Subtype'] == '/Image':
-            image_obj = obj
+        if '/Filter' not in image_obj:
+            continue
 
-            if '/Filter' not in image_obj:
-                continue
+        # FIXME: to improve *a lot*
+        if (image_obj.Filter != '/DCTDecode' and
+           not (isinstance(image_obj.Filter, pikepdf.Array) and
+                len(image_obj.Filter) == 1 and
+                image_obj.Filter[0] == '/DCTDecode')):
+            continue
 
-            # FIXME: to improve *a lot*
-            if (image_obj.Filter != '/DCTDecode' and
-               not (isinstance(image_obj.Filter, pikepdf.Array) and
-                    len(image_obj.Filter) == 1 and
-                    image_obj.Filter[0] == '/DCTDecode')):
-                continue
+        if image_obj.ColorSpace not in ('/DeviceRGB', '/DeviceGray'):
+            continue
 
-            if image_obj.ColorSpace not in ('/DeviceRGB', '/DeviceGray'):
-                continue
+        logging.debug('Found a JPEG as %s', image_obj.ColorSpace)
 
-            logging.debug('Found a JPEG as %s', image_obj.ColorSpace)
+        tempname = os.path.join(tmpdirname, 'foobarbaz.jpg')  # FIXME: change this
+        source = open(tempname, 'wb')
 
-            tempname = os.path.join(tmpdirname, 'foobarbaz.jpg')  # FIXME: change this
-            source = open(tempname, 'wb')
+        size_before = source.write(image_obj.read_raw_bytes())
+        logging.debug('Wrote %d bytes to the tempfile %s.', size_before, tempname)
+        source.close()
 
-            size_before = source.write(image_obj.read_raw_bytes())
-            logging.debug('Wrote %d bytes to the tempfile %s.', size_before, tempname)
-            source.close()
+        # print('Calling jpgcrush...')
+        subprocess.check_call(['jpgcrush', tempname])
+        # print('Return code was: %d.' % ret)
 
-            # print('Calling jpgcrush...')
-            subprocess.check_call(['jpgcrush', tempname])
-            # print('Return code was: %d.' % ret)
+        # Unfortunatel, the -purejpg of jhead is too aggressive and may
+        # strip way too much to the point of modifying the image, in some
+        # cases.
+        logging.debug('Calling jhead...')
+        subprocess.check_call(['jhead', '-dt', '-dc', '-de', source.name])
+        # print('Return code was: %d.' % ret)
 
-            # Unfortunatel, the -purejpg of jhead is too aggressive and may
-            # strip way too much to the point of modifying the image, in some
-            # cases.
-            logging.debug('Calling jhead...')
-            subprocess.check_call(['jhead', '-dt', '-dc', '-de', source.name])
-            # print('Return code was: %d.' % ret)
+        targetfn = open(tempname, 'rb')
+        target = targetfn.read()
 
-            targetfn = open(tempname, 'rb')
-            target = targetfn.read()
+        size_after = len(target)
+        logging.debug('Read back %d bytes from the tempfile %s.', size_after, tempname)
+        image_obj.write(target, filter=pikepdf.Name('/DCTDecode'))
+        logging.debug('The image is back on the PDF file.')
 
-            size_after = len(target)
-            logging.debug('Read back %d bytes from the tempfile %s.', size_after, tempname)
-            image_obj.write(target, filter=pikepdf.Name('/DCTDecode'))
-            logging.debug('The image is back on the PDF file.')
-
-            total_savings += size_before - size_after
+        total_savings += size_before - size_after
 
     final_filename = os.path.splitext(pdf_name)[0] + '.jpg.pdf'
     logging.info('Saved %d bytes to create %s.', total_savings, final_filename)
