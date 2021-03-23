1f4e2775383eab1cf6929e50ddff8decbfa48d72
Rogério Brito <rbrito@gmail.com>
2021-03-23 19:57:31 -0300

    wipe_image: Try to automatically detect DPIs.

--- wipe_image.py
+++ wipe_image.py
@@ -7,14 +7,11 @@ from PIL import Image, ImageDraw
 from ocrmypdf.leptonica import Pix
 from tqdm import tqdm
 
-
 WHITE = "#fff"
-BORDER = 10
-DPI = 600
 
 
-# Inspiration taken from jbig2enc, which uses leptonica's
-# pixThresholdToBinary function on a PIX.
+# Method taken from jbig2enc, which uses leptonica's pixThresholdToBinary
+# function on a PIX.
 def threshold_image(in_im, threshold=188):
     '''
     in_im (should be) a grayscale PIL.Image. The returned image is a B/W
@@ -41,16 +38,26 @@ def despeckle(im, size=2):
 
 def wipe_borders(in_file, out_file):
     with Image.open(in_file) as im:
+
+        xdpi, ydpi = im.info['dpi']
+
+        # We only want to deal with images that have the same DPIs
+        # vertically and horizontally.
+        assert xdpi == ydpi
+
+        dpi = xdpi
+        border = dpi / 10  # hunch
+
         draw = ImageDraw.Draw(im)
 
         # FIXME: Does the interpretation of top, left, bottom and right
         # remain the same if the origin is moved from top-left to bottom-left?
-        draw.rectangle([0, 0, im.width, BORDER], fill=WHITE)  # top
-        draw.rectangle([0, im.height - BORDER, im.width, im.height], fill=WHITE)  # bottom
+        draw.rectangle([0, 0, im.width, border], fill=WHITE)  # top
+        draw.rectangle([0, im.height - border, im.width, im.height], fill=WHITE)  # bottom
 
         # If not desired, comment these
-        draw.rectangle([0, 0, BORDER, im.height], fill=WHITE)  # left
-        draw.rectangle([im.width - BORDER, 0, im.width, im.height], fill=WHITE)  # right
+        draw.rectangle([0, 0, border, im.height], fill=WHITE)  # left
+        draw.rectangle([im.width - border, 0, im.width, im.height], fill=WHITE)  # right
 
         despeckled = threshold_image(im)
         deskewed = deskew(despeckled)
@@ -58,7 +65,7 @@ def wipe_borders(in_file, out_file):
         # deskewed = deskew(threshold_image(im))
         # im.save(out_file, dpi=(DPI, DPI), resolution=DPI, compression="packbits")  # for PNGs: compress_level=1
 
-        deskewed.save(out_file, dpi=(DPI, DPI), resolution=DPI, compression='group4')
+        deskewed.save(out_file, dpi=(dpi, dpi), resolution=dpi, compression='group4')
         deskewed.close()
         # despeckled.save(out_file, dpi=(DPI, DPI), resolution=DPI, compression='group4')
         despeckled.close()
